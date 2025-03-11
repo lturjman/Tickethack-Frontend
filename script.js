65bfd100db98ff2baeda232a3d9e8751123394ba
=megahertz77 <megahertz77@gmail.com>
2025-03-11 13:42:15 +0100

    homepage bien avancé, mais statique encore sans code dans le script.

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ fetch("http://localhost:3000/api/message")
   .then((response) => response.json())
   .then((data) => console.log(data.message))
   .catch((error) => console.error("Erreur :", error));
+
+  
