de8223e1646661c492e119f90a1a31179021e17f
Laura Turjman <contact@lauraturjman.fr>
2025-03-11 10:01:39 +0100

    confit frontend

---
+++ script.js
@@ -0,0 +1,4 @@
+fetch("http://localhost:3000/api/message")
+  .then((response) => response.json())
+  .then((data) => console.log(data.message))
+  .catch((error) => console.error("Erreur :", error));
